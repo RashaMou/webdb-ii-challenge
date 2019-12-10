f87067bd048704ba55ce8571b34fe9cb97e51868
Rasha Moumneh <rasha@fastmail.com>
2019-12-10 15:47:55 -0500

    Add update and delete functionality to cars table

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -37,4 +37,34 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  const carUpdate = req.body;
+
+  knex("cars")
+    .where({ id: req.params.id })
+    .update(carUpdate)
+    .then(count => {
+      if (count > 0) {
+        res.status(201).json({ message: `${count} record(s) updated` });
+      } else {
+        res.status(404).json("Record not found");
+      }
+    })
+    .catch(error => {
+      res.status(500).json("Error updating record");
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  knex("cars")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json({ message: `${count} record(s) deleted` });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error deleting the car" });
+    });
+});
+
 module.exports = router;
