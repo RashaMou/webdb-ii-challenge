b1dc25b6ce58026b63c7f091100c99a837208d3b
Rasha Moumneh <rasha@fastmail.com>
2019-12-10 15:45:20 -0500

    Add create and read operations on cars table

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -2,4 +2,39 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../data/dbConfig");
 
+router.get("/", (req, res) => {
+  knex
+    .select("*")
+    .from("cars")
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(error => {
+      res.status(500).json("Error retrieving cars");
+    });
+});
+
+router.post("/", (req, res) => {
+  const carData = req.body;
+  knex("cars")
+    .insert(carData, "id")
+    .then(ids => {
+      const id = ids[0];
+      knex("*")
+        .from("cars")
+        .where({ id })
+        .first()
+        .then(car => {
+          res.status(201).json(car);
+        })
+        .catch(error => {
+          res.status(500).json("Error retrieving car with specified id");
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json("Error adding car");
+    });
+});
+
 module.exports = router;
